20ff562e2ecea50172e1aa7bde29182dc8abed7f
YongMinSun <ddoni1010@gmail.com>
2019-11-13 20:38:59 +0900

    TedPemission을 이용한 권한 획득 예제

--- app/src/main/java/com/tioeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tioeun/a20191113_01_libraries/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.tioeun.a20191113_01_libraries
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -12,18 +17,38 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        Toast.makeText(mContext, "테스트용 토스트", Toast.LENGTH_SHORT)
+//        Toast.makeText(mContext, "테스트용 토스트", Toast.LENGTH_SHORT).show()
 
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+        callBtn.setOnClickListener {
 
+            var permissionListener = object : PermissionListener{
+                override fun onPermissionGranted() {
+                    var uri = Uri.parse("tel: ${phoneTxt.text.toString().replace("-", "")}")
+                    var intent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(intent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 설정해야 전화 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("전화 권한을 설정해야 바로 연결 가능합니다.")
+                .setDeniedMessage("설정 > 권한에서 허용 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
     }
 
     override fun setValues() {
-        var url = "https://pbs.twimg.com/profile_images/1153500695506460672/U9lK1j6K_400x400.jpg"
+        var url = "http://image.kyobobook.co.kr/newimages/giftshop_new/goods/400/1539/S1458632370750.jpg"
         Glide.with(this).load(url).into(profileImg)
     }
 
