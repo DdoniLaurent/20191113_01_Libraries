0984c5d3a3445b95f5575eb0ed690f5240f7bd2e
YongMinSun <ddoni1010@gmail.com>
2019-11-13 19:54:20 +0900

    Glide를 활용한 예제

--- app/src/main/java/com/tioeun/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tioeun/a20191113_01_libraries/MainActivity.kt
@@ -3,6 +3,8 @@ package com.tioeun.a20191113_01_libraries
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -21,7 +23,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        var url = "https://pbs.twimg.com/profile_images/1153500695506460672/U9lK1j6K_400x400.jpg"
+        Glide.with(this).load(url).into(profileImg)
     }
 
 
